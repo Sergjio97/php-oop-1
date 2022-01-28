d1b433a0ca33be427ad77b0294b8b1440f05f6d2
Sergjio97 <89703124+Sergjio97@users.noreply.github.com>
2022-01-28 18:55:58 +0100

    generazione attributo trama

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ $Films = [
     new Movies("Io sto con gli ippopotami", "Italiano", "commedia/avventura/azione", 1979, 109),
 ];
 
+//trame 
+$AltrimentiCiArrabbiamo = $Films[0]->trama = "Ben e Kid (Bud Spencer e Terence Hill), un meccanico e un camionista, sono grandi amici, ma diventano acerrimi rivali quando si tratta di corse automobilistiche. Un giorno si trovano uno contro l’altro in una gara di rallycross, in cui il primo premio è una Dune Buggy tutta rossa con cappottina gialla.";
+$LoChiamavanoTrinità = $Films[1]->trama = "Il pistolero Trinità, pigro ma dal cuore d'oro e particolarmente abile con la pistola, vaga nel deserto sdraiato su una treggia trainata dal suo cavallo. Giunto in una squallida locanda, si scontra con due cacciatori di taglie e sottrae loro un messicano che ha una taglia sulla testa. Continuando il suo viaggio, giunge in un paese dove trova suo fratello, detto Bambino, delinquente e ladro di cavalli divenuto inaspettatamente sceriffo. ";
+$IoStoConGliIppopotami = $Films[2]->trama = "Africa coloniale britannica, 1950. Nella Rhodesia per lavoro, Tom organizza safari per turisti, armandoli a loro insaputa con fucili caricati a salve (eccetto il suo), poiché comunque ama e rispetta gli animali. L'uomo è stato cresciuto con suo cugino Slim da una donna nativa africana, dopo che suo padre e suo zio sono morti.
+Con l'arrivo di Slim, fannullone giramondo e rubacuori, iniziano le scaramucce tra i due che si concludono quando diventano soci nell'acquisto di un nuovo pullman per i safari. ";
+
+
 ?>
 
 <!DOCTYPE html>
